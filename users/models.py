5a7db8653aadedd87ec5b6170fba0b030560a9f2
WandersoNoleto <onlywanderson@gmail.com>
2024-05-25 14:03:17 -0300

    feat: (001) auth-users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPES = (
+        ('clinic', 'Clinic'),
+        ('doctor', 'Doctor'),
+        ('receptionist', 'Receptionist'),
+        ('partner_optic', 'Partner Optic'),
+    )
+    user_type = models.CharField(max_length=20, choices=USER_TYPES)
+
+class Clinic(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, null=True, blank=True) 
+
+class Receptionist(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+
+class PartnerOptic(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
