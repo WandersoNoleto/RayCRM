a8df298663a73bd083e65604aa920411cb17a0f0
WandersoNoleto <onlywanderson@gmail.com>
2024-05-27 21:33:08 -0300

    feat: (002) auth-users

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from .  import views
 
 urlpatterns = [
-    path('login/', views.login, name="login")
+    path('login/', views.login_view, name="login"),
+    path('login-check/', views.login_check, name="login_check"),
+    path('register/', views.register_view, name="register"), 
+    path('user-storage/', views.user_storage, name="user_storage")
 ]
