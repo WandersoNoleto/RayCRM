191cfb77afe615729cae54cfa4c6f52fb79ee561
WandersoNoleto <onlywanderson@gmail.com>
2024-06-01 15:00:13 -0300

    feat: possible to add pacient

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,5 +3,6 @@ from .  import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('pacients/', views.view_pacients, name="view_pacients")
+    path('pacients/', views.view_pacients, name="view_pacients"),
+    path('add-pacient/', views.add_pacient, name="add_pacient")
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Patient
 
@@ -8,13 +8,34 @@ def home(request):
 
 @login_required
 def view_pacients(request):
-    patients =  Patient.objects.all()
-    print(patients)
+    patients = Patient.objects.all().order_by('name')
     context = {
         'patients': patients,
     }
 
     return render(request, 'pacients.html', context=context)
 
+@login_required
+def add_pacient(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        birth_date = request.POST.get('birth_date')
+        address = request.POST.get('address')
+        clinic = request.user.clinic
+
+        print(name, phone, birth_date, address, clinic)
+
+        patient = Patient(
+            name=name,
+            phone=phone, 
+            birth_date=birth_date,
+            address=address,
+            clinic=clinic
+        )
+
+        patient.save()
+
+    return redirect('view_pacients')
 
 
